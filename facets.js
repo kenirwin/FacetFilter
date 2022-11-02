ee16052df70343b4426fde5b0acb17d5acca7d4e
Ken Irwin <ken@kenirwin.net>
2022-11-02 16:29:47 -0400

    initial working version - text facets

---
+++ facets.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  let facets = [];
+  $('.facets').each(function () {
+    facets.push($(this).attr('data-facet'));
+  });
+
+  $('input').change(function () {
+    $('#objects li').each(function () {
+      $(this).attr('data-toshow', true);
+    });
+    facets.forEach(function (f) {
+      filterOutItems = $(
+        '.facets[data-facet=' + f + '] input[type=checkbox]:not(:checked)'
+      );
+      let filterOutValues = [];
+      $(filterOutItems).each(function () {
+        filterOutValues.push($(this).attr('id'));
+      });
+      attrName = 'data-' + f;
+      // find hidable items
+      filterOutValues.forEach(function (val) {
+        $('#objects li[' + attrName + '=' + val + ']').attr(
+          'data-toshow',
+          false
+        );
+      });
+      $('#objects li[data-toshow=false]').hide();
+      $('#objects li[data-toshow=true]').show();
+      console.log(f + ':', filterOutValues);
+    });
+  });
+});
