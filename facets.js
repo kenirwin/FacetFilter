3db181e078d41d7d7d0be06d96d1e7e97cb3743d
Ken Irwin <ken@kenirwin.net>
2022-11-02 16:45:29 -0400

    restructured facets.js to plan for future features

--- facets.js
+++ facets.js
@@ -1,32 +1,38 @@
 $(document).ready(function () {
+  $('input').change(function () {
+    setAllToShow();
+    processTextFacets();
+    showHide();
+  });
+});
+
+function setAllToShow() {
+  $('#objects li').each(function () {
+    $(this).attr('data-toshow', true);
+  });
+}
+function showHide() {
+  $('#objects li[data-toshow=false]').hide();
+  $('#objects li[data-toshow=true]').show();
+}
+function processTextFacets() {
   let textFacets = [];
   $('.facets-text').each(function () {
     textFacets.push($(this).attr('data-facet'));
   });
-
-  $('input').change(function () {
-    $('#objects li').each(function () {
-      $(this).attr('data-toshow', true);
+  textFacets.forEach(function (f) {
+    filterOutItems = $(
+      '.facets-text[data-facet=' + f + '] input[type=checkbox]:not(:checked)'
+    );
+    let filterOutValues = [];
+    $(filterOutItems).each(function () {
+      filterOutValues.push($(this).attr('id'));
     });
-    textFacets.forEach(function (f) {
-      filterOutItems = $(
-        '.facets-text[data-facet=' + f + '] input[type=checkbox]:not(:checked)'
-      );
-      let filterOutValues = [];
-      $(filterOutItems).each(function () {
-        filterOutValues.push($(this).attr('id'));
-      });
-      attrName = 'data-' + f;
-      // find hidable items
-      filterOutValues.forEach(function (val) {
-        $('#objects li[' + attrName + '=' + val + ']').attr(
-          'data-toshow',
-          false
-        );
-      });
-      $('#objects li[data-toshow=false]').hide();
-      $('#objects li[data-toshow=true]').show();
-      console.log(f + ':', filterOutValues);
+    attrName = 'data-' + f;
+    // find hidable items
+    filterOutValues.forEach(function (val) {
+      $('#objects li[' + attrName + '=' + val + ']').attr('data-toshow', false);
     });
+    console.log(f + ':', filterOutValues);
   });
-});
+}
