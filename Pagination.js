af46b389a485cd728edcf9402676f715cb95313e
Ken Irwin <ken@kenirwin.net>
2022-11-21 15:49:00 -0500

    disables prev/next buttons when they run out

--- Pagination.js
+++ Pagination.js
@@ -76,15 +76,24 @@ function createPagination(pages, paginationDivId) {
 
 function updatePageControls(page) {
   console.log('updatePageControls', page);
-  page = page + 1; // skip the previous button
-  numPages = $('.page-item').length; // skip the previous and next buttons
+  let buttonIndex = page + 1; // skip the previous button
+  let numPages = getTotalPages(); // skip the previous and next buttons
+  let maxButtonIndex = numPages + 1;
+  nextButtonIndex = maxButtonIndex + 1;
   console.log('page', page, 'of', numPages);
   $('.page-item').removeClass('active');
-  $('.page-item:nth-child(' + page + ')').addClass('active');
-  if (page == 2) {
+  $('.page-item:nth-child(' + buttonIndex + ')').addClass('active');
+  if (buttonIndex == 2) {
     $('.page-item:nth-child(1)').addClass('disabled');
-  } else if (page == numPages) {
-    $('.page-item:nth-child(' + numPages + ')').addClass('disabled');
+  } else {
+    $('.page-item:nth-child(1)').removeClass('disabled');
+  }
+
+  console.log('buttonIndex', buttonIndex, 'maxButtonIndex', maxButtonIndex);
+  if (buttonIndex == maxButtonIndex) {
+    $('.page-item:nth-child(' + nextButtonIndex + ')').addClass('disabled');
+  } else {
+    $('.page-item:nth-child(' + numPages + ')').removeClass('disabled');
   }
 }
 
