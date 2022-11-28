a4254d1283d99ae46b2fa5f719257ad6f22cb378
Ken Irwin <ken@kenirwin.net>
2022-11-28 17:23:11 -0500

    support reading url params for "text" type fields: gender=Non-Binary

--- UrlFilters.js
+++ UrlFilters.js
@@ -1,10 +1,10 @@
-function urlFilters(params) {
+function urlFilters(params, conf) {
   const urlParams = new URLSearchParams(params);
   for (const key of urlParams.keys())
-    applyUrlFilter(key, urlParams.getAll(key));
+    applyUrlFilter(key, urlParams.getAll(key), conf);
 }
 
-function applyUrlFilter(key, values) {
+function applyUrlFilter(key, values, conf) {
   let facet = $('#facet-' + key);
   let type = facet.data('type');
   console.log(key, values, type);
@@ -13,10 +13,26 @@ function applyUrlFilter(key, values) {
       applyTagFilter(key, value);
     });
   }
+  if (type == 'text') {
+    values.forEach(function (value) {
+      applyCheckboxFilter(key, values[0], conf);
+    });
+  }
 }
 
 function applyTagFilter(facet, value) {
   //   console.log('applyTagFilter', facet, value);
   let facetId = '#facet-' + facet;
   $(facetId + ' .facet-tag[data-value="' + value + '"]').click();
+}
+
+function applyCheckboxFilter(facet, value, conf) {
+  console.log('applyCheckboxFilter', facet, value);
+  let facetId = '#' + facet + '-' + value;
+  // $(facetId + ' .facet-checkbox[data-value="' + value + '"]').click();
+  console.log('unchecking field=' + facet);
+  console.log('checking' + facetId);
+  $('input[data-field="' + facet + '"]').prop('checked', false);
+  $(facetId).prop('checked', true);
+  $(conf.facetDivId).trigger('facetFilter.update');
 }
